a338dc186450c34f57264ffa1734b2cad2899451
bel-salm <almeidasalesisabel@gmail.com>
2024-10-17 11:20:18 -0300

    chore: update days

--- src/bookings.js
+++ src/bookings.js
@@ -140,16 +140,6 @@ const bookings = [
       middayCheckout: false,
     },
     {
-      from: '10-19-2024',
-      to: '10-20-2024',
-      middayCheckout: false,
-    },
-    {
-      from: '10-26-2024',
-      to: '10-27-2024',
-      middayCheckout: false,
-    },
-    {
       from: '11-01-2024',
       to: '11-03-2024',
       middayCheckout: false,
